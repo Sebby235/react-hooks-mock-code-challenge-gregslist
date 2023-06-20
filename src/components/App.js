c9a2c4d9c741ddebd3311627e153e638e131d35b
bonbongiraffe <francescowai@yahoo.com>
2023-06-20 10:49:08 -0400

    core deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+  const [filteredList, setFilteredList] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then((listingsObj) => {
+        setListings(listingsObj)
+        setFilteredList(listingsObj)
+      })
+  }, [])
+
+  const handleDelete = (deleteID) => {
+    //remove deleted listing from filtered list
+    setFilteredList(filteredList.filter((listing) => listing.id !== deleteID))
+    //remove deleted listing from un-filtered list
+    setListings(listings.filter((listing) => listing.id !== deleteID))
+    //persisting delete
+    fetch(`http://localhost:6001/listings/${deleteID}`,{
+      method:'DELETE',
+      headers: {"Content-Type": "application/json"}
+    })
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const handleSearchSubmit = () => {
+    setSearch("")
+    if (search === "")
+      setFilteredList(listings)
+    else
+      setFilteredList(filteredList.filter((listing) => listing.description.toLowerCase().includes(search.toLowerCase())))
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header search={search} handleSearch={handleSearch} handleSearchSubmit={handleSearchSubmit}/>
+      <ListingsContainer listings={filteredList} handleDelete={handleDelete}/>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Search() {
+function Search({ search, handleSearch, handleSearchSubmit}) {
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    handleSearchSubmit()
   }
 
   return (
@@ -12,8 +12,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={search}
+        onChange={handleSearch}
       />
       <button type="submit">🔍</button>
     </form>
